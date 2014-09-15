e2afdc025f049f6c8de5adf50c611060043e5d46
Matthew Jee <admin@marmphco.com>
2014-09-14 22:50:53 -0700

    Feature: Load stochastic series from URL
    The experimenter can now specify a URL pointing to a CSV
    file containing stochastic series' that make up the
    stochastic functions used to determine stock payoffs.

--- market/start.js
+++ market/start.js
@@ -1,15 +1,71 @@
-Redwood.factory("PortfolioAllocation", function() {
-  return {
+Redwood.factory("PortfolioAllocation", ["$q", "$http", function($q, $http) {
+  var api = {};
 
-    defaultStochasticFunction: function(x) {
-      return Math.random() * 1.0 + 0.5;
-    },
-  }
-});
+  api.defaultStochasticFunction = function(x, round) {
+    return Math.random() * 1000 + 500;
+  };
+
+  // returns a promise that passes a function to its then callback
+  api.createStochasticFunction = function(configString) {
+    var matchURL = configString.match(/http.*/);
+    if (matchURL) {
+      // get stochastic series csv file from URL
+      var fullURL = configString;
+      var deferred = $q.defer();
+
+      $http({method: "GET", url: fullURL})
+        .success(function(data, status, headers, config) {
+          // build stochastic series (simple csv parsing with String.split)
+          var rows = data.split("\n");
+
+          // create series arrays
+          var stochasticSeries = [];
+          for (var i = 0; i < rows[0].split(",").length; i++) {
+            stochasticSeries[i] = [];
+          }
+
+          // fill arrays with csv data
+          for (var i = 0; i < rows.length; i++) {
+            var cells = rows[i].split(",");
+            for (var j = 0; j < cells.length; j++) {
+              stochasticSeries[j][i] = parseFloat(cells[j]);
+            }
+          }
+          console.log(stochasticSeries);
+
+          var stochasticFunction = function(x, round) {
+            // normalized to 0.5 to 1.5, for now
+            return stochasticSeries[round % stochasticSeries.length][x] / 1000.0;
+          };
+
+          deferred.resolve(stochasticFunction);
+        })
+        .error(function(data, status, headers, config) {
+          console.log("(;-;) " + data);
+          deferred.resolve(status);
+        });
+
+      return deferred.promise;
+
+    } else if (configString) {
+      var deferred = $q.defer();
+      deferred.resolve(new Function("x", "round", "return " + configString));
+      return deferred.promise;
+
+    } else {
+      var deferred = $q.defer();
+      deferred.resolve(api.defaultStochasticFunction);
+      return deferred.promise;
+    }
+  };
+
+  return api;
+}]);
 
 Redwood.controller("SubjectCtrl", ["$scope", "RedwoodSubject", "$timeout", "PortfolioAllocation", function($scope, rs, $timeout, experiment) {
   // Initialize some scope variables (the reset are initialized in on_load)
 
+  $scope.isLoadingStochasticSeries = true;
   $scope.isSimulating = false;
   $scope.plotNeedsRedraw = false;
 
@@ -31,7 +87,6 @@ Redwood.controller("SubjectCtrl", ["$scope", "RedwoodSubject", "$timeout", "Port
 
       if ($scope.isSimulating) {
         // hack to get day simulation "timeout chain" to resume.
-        console.log("resuming simulation timeout chain");
         var lastSimulatedDay = $scope.stochasticValues[$scope.round].length - 1;
         $timeout(simulateDay(lastSimulatedDay + 1), $scope.config.secondsPerDay * 1000);
       } else {
@@ -60,11 +115,24 @@ Redwood.controller("SubjectCtrl", ["$scope", "RedwoodSubject", "$timeout", "Port
     return function() {
       if (day < $scope.config.daysPerRound) {
 
-        rs.trigger("simulatedDay", {
-          round: $scope.round,
-          day: day,
-          value: $scope.config.stochasticFunction(day)
-        });
+        var triggerNextDay = function() {          
+          rs.trigger("simulatedDay", {
+            round: $scope.round,
+            day: day,
+            value: $scope.config.stochasticFunction(day, $scope.round)
+          });
+        };
+
+        // wait for the stochastic function to load if necessary
+        // (If the stochastic function is a promise, then it hasn't loaded yet)
+
+        if (typeof $scope.config.stochasticFunction.then === "function") {
+          if (rs.is_realtime) {
+            $scope.config.stochasticFunction.then(triggerNextDay);
+          }
+        } else {
+          triggerNextDay();
+        }
 
       } else {
         // compute round results
@@ -99,8 +167,18 @@ Redwood.controller("SubjectCtrl", ["$scope", "RedwoodSubject", "$timeout", "Port
       bondReturn: rs.config.bondReturn         || 0.2,
       plotMinY: rs.config.plotMinY             || 0.5,
       plotMaxY: rs.config.plotMaxY             || 1.5,
-      stochasticFunction: new Function("x", "return " + rs.config.stochasticFunction) || experiment.defaultStochasticFunction,
+      stochasticFunction: null,
     };
+
+    // Load stochastic function (may be a dropbox URL)
+    // Don't allow allocation confirmation until the stochastic function has been loaded.
+    // Set the value of the stochastic function to the promise so that other things
+    // that need to use it an schedule a then handler on the promise.
+    $scope.config.stochasticFunction = experiment.createStochasticFunction(rs.config.stochasticFunction)
+      .then(function(stochasticFunction) {
+        $scope.isLoadingStochasticSeries = false;
+        $scope.config.stochasticFunction = stochasticFunction;
+      });
 
     $scope.allocation = {
       stock: $scope.config.wealthPerRound/2,
